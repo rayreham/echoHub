d52188d702a053ee87b4a3c48736c7e2c9c15115
rayreham <rayham.ghoneim@gmail.com>
2024-04-30 20:01:34 -0400

    Cleaning files and adding buttons to play and pause demo

--- src/components/MainHeroVideo.tsx
+++ src/components/MainHeroVideo.tsx
@@ -1,20 +1,25 @@
-import React, { useRef } from 'react';
+import React, { useRef, useState } from 'react';
+
 import config from '../config/index.json';
 
 const MainHeroVideo = () => {
   const { mainHero } = config;
   const videoRef = useRef<HTMLVideoElement>(null);
+  const [isPlaying, setIsPlaying] = useState<boolean>(false);
 
   const togglePlayPause = () => {
     if (videoRef.current?.paused) {
       videoRef.current.play();
+      setIsPlaying(true);
     } else {
       videoRef.current?.pause();
+      setIsPlaying(false);
     }
   };
 
   return (
-    <div className="lg:absolute lg:inset-y-0 lg:right-0 lg:w-1/2 flex items-center justify-center">
+    <div className="lg:absolute lg:inset-y-0 lg:right-0 lg:w-1/2 flex items-center justify-center flex-col">
+      <div className="text-black text-2xl font-bold mb-4">Demo</div>
       <video
         ref={videoRef}
         className="h-56 md:h-96 lg:w-full"
@@ -25,6 +30,12 @@ const MainHeroVideo = () => {
         <source src={mainHero.video} type="video/mp4" />
         Your browser does not support the video tag.
       </video>
+      <button
+        className={`px-4 py-2 border border-transparent text-base font-medium rounded-md text-white bg-primary transition duration-300 ease-in-out hover:bg-white hover:text-primary hover:border-primary mt-4`}
+        onClick={togglePlayPause}
+      >
+        {isPlaying ? <span>PAUSE</span> : <span>PLAY DEMO</span>}
+      </button>
     </div>
   );
 };
